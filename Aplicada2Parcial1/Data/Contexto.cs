45c5271e57d4797fab4644a3ed4438c8d2c20fc6
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-02-04 22:10:37 -0400

    Cargando la estructura requerida

---
+++ Aplicada2Parcial1/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicada2Parcial1.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database= Parcial1Db; Trusted_Connection = True; ");
+        }
+    }
+}
